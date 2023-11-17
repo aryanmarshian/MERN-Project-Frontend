08720fdbe4c6fef731e528caf77e219ee4f4cd5d
unknown <Ishwaq>
2023-11-17 18:44:59 +0530

    First commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,52 @@
+// undraw.co to get svg images
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium MIASA',
+    headline: 'Extra Access to Premium features',
+    description: 'Get access to our premium subscirption to unlock more!',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'MIASA ABOUT',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Discover Miasa',
+    headline: 'Effortless Administration At Your Fingertips',
+    description: 'Manage student records, attendance, and grades effortlessly',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'GugaDiscover',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join Us',
+    headline: 'Creating an account is extremely easy',
+    description: "Get everything set up and ready in under 10 minutes. All you need to do is add your Credentials and Education Institute and you're ready to go.",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'GugaSignUp',
+    dark: false,
+    primary: false,
+    darkText: true
+}
