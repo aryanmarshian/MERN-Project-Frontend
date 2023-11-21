5bf6942efe88e72a01c52b0788d79438f199cd32
Srichaitanya C <73554283+ZeroWing07@users.noreply.github.com>
2023-11-21 08:14:09 +0530

    ChooseUser.js

--- src/pages/ChooseUser.js
+++ src/pages/ChooseUser.js
@@ -28,7 +28,7 @@ const ChooseUser = ({ visitor }) => {
   const navigateHandler = (user) => {
     if (user === "Admin") {
       if (visitor === "guest") {
-        const email = "yogendra@12"
+        const email = "csrichaitanya@12"
         const fields = { email, password }
         setLoader(true)
         dispatch(loginUser(fields, user))
@@ -41,7 +41,7 @@ const ChooseUser = ({ visitor }) => {
     else if (user === "Student") {
       if (visitor === "guest") {
         const rollNum = "1"
-        const studentName = "Dipesh Awasthi"
+        const studentName = "Student07"
         const fields = { rollNum, studentName, password }
         setLoader(true)
         dispatch(loginUser(fields, user))
@@ -53,7 +53,7 @@ const ChooseUser = ({ visitor }) => {
 
     else if (user === "Teacher") {
       if (visitor === "guest") {
-        const email = "tony@12"
+        const email = "teacher@07"
         const fields = { email, password }
         setLoader(true)
         dispatch(loginUser(fields, user))
